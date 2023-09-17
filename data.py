ceabd5290df5fa8bbaa4c5f610aa323961283faf
EllaYu310 <ellay9@nycstudents.net>
2023-09-17 18:28:41 -0400

    Create data.py

---
+++ data.py
@@ -0,0 +1,22 @@
+x = 3
+y = float(3)
+print(x,y)
+
+#tip calculator
+#bill=float(input("Enter bill amount:$"))
+#tip_percentage=int(input("Enter tip percentage:%"))
+#total_amount= (bill+((tip_percentage*bill)/100))
+#print(f"total_amount:{total_amount}")
+
+#values = [1,2.23,5,7,2,30,15]
+#print (values[3])
+
+x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z)
+
+sentence= (input("Enter a sentence:"))
+word_count= sentence.split()
+print(word_count)
