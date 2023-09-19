5dfbdab03ff5ea9fe903e94fe492ac05b75359e8
EllaYu310 <ellay9@nycstudents.net>
2023-09-18 21:40:02 -0400

    Update MadLibs.py

--- MadLibs.py
+++ MadLibs.py
@@ -1,8 +1,8 @@
 Verb_1=(input("Enter your first verb:"))
 Verb_2=(input("Enter your second verb:"))
-Noun_1=(input("Enter your first noun:"))
+Noun_1=(input("Enter a plural noun:"))
 Number_1=int(input("Enter a whole number:#"))
 Celeberty_guest=(input("Enter the name of a celeberty:"))
 
-Madlib=(f"I was {Verb_1}, through the snow. I wished I {Verb_2} through the snow instead. Though as I continued my journey I saw {Celeberty_guest}, walking on {Number_1} {Noun_1}!")
+Madlib=(f"I was {Verb_1} through the snow. I wished I was {Verb_2} through the snow instead. Though as I continued my journey I saw {Celeberty_guest}, walking on {Number_1} {Noun_1}!")
 print(f"Madlib:{Madlib}")
