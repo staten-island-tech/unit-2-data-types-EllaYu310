d9f101facac7ad92bbd8831782a0a92ddd0cf82e
EllaYu310 <ellay9@nycstudents.net>
2023-09-19 14:00:57 -0400

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
-x = 3
-y = float(3)
-print(x,y)
+#x = 3
+#y = float(3)
+#print(x,y)
 
 #tip calculator
 #bill=float(input("Enter bill amount:$"))
@@ -11,12 +11,38 @@ print(x,y)
 #values = [1,2.23,5,7,2,30,15]
 #print (values[3])
 
-x = "this is a thing"
-y= x.split( )
-z = y[0]
-print(y)
-print(z)
+#x = "this is a thing"
+#y= x.split( )
+#z = y[0]
+#print(y)
+#print(z)
 
-sentence= (input("Enter a sentence:"))
-word_count= sentence.split()
-print(word_count)
+#challenge1
+#sentence= (input("Enter a sentence:"))
+#word_count= sentence.split()
+#print(word_count)
+
+#Number_of_monkeys = input("How many monkeys are there?:")
+#if Number_of_monkeys == "50": #if you use if you put the thing you want
+    #print("correct")
+#else: #else is used to show that you dont want it
+    #print("incorrect")
+    
+#x = "test"
+#print(f"hello {x}")
+
+#temp = 75
+#if temp > 68:
+#    print('warm')
+#elif temp == 68:
+#    print('perfect')
+#else:
+#    print('cold')
+    
+##Challenge odd or even
+number=7
+if number == base[0,2,4,6,8]: 
+    print("Even")
+else:
+    print("Odd")
+##Challenge bill
