1529255f02be7d17ef6a4af268302dcfa5179289
EllaYu310 <ellay9@nycstudents.net>
2023-09-21 13:59:23 -0400

    Update data.py

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
-#tip calculator
-#bill=float(input("Enter bill amount:$"))
-#tip_percentage=int(input("Enter tip percentage:%"))
-#total_amount= (bill+((tip_percentage*bill)/100))
-#print(f"total_amount:{total_amount}")
+#Challenge tip calculator
+bill=float(input("Enter bill amount:$"))
+tip_percentage=int(input("Enter tip percentage:%"))
+total_amount= (bill+((tip_percentage*bill)/100))
+print(f"total_amount:{total_amount}")
 
 #values = [1,2.23,5,7,2,30,15]
 #print (values[3])
@@ -14,33 +14,23 @@
 #print(z)
 
 #challenge1
-#sentence= (input("Enter a sentence:"))
-#word_count= sentence.split()
-#print(word_count)
+sentence= (input("Enter a sentence:"))
+word_count= sentence.split()
+print(word_count)
 
 #Number_of_monkeys = input("How many monkeys are there?:")
 #if Number_of_monkeys == "50": #if you use if you put the thing you want
     #print("correct")
 #else: #else is used to show that you dont want it
     #print("incorrect")
-    
-#x = "test"
-#print(f"hello {x}")
 
-#temp = 75
-#if temp > 68:
-#    print('warm')
-#elif temp == 68:
-#    print('perfect')
-#else:
-#    print('cold')
-    
 ##Challenge odd or even
-#number=7
-#if number%2 == 0: 
-#    print("Even")
-#else:
-#    print("Odd")
+number=7
+if number%2 == 0: 
+    print("Even")
+else:
+    print("Odd")
+    
 ##Challenge bill
 bill=(input("Enter bill amount:$"))
 service=(input("What would you rate the service? bad, okay, good, or great?:"))
@@ -54,3 +44,9 @@ elif service == "great":
     print("25% tip?")
 else:
     print("Please rate our service.")
+    
+##Challenge Finding factors
+number=input("Enter a number")
+
+
+##Challenge 2 arguments, find factor
