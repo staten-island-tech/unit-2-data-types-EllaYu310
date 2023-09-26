30db88261f80e90bebb553218a699576800abcf2
EllaYu310 <ellay9@nycstudents.net>
2023-09-26 13:59:51 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-def gcf(a,b):
-    if b==0:
-        return a
-    else:
-        return gcf(b, a%b)
-a = int(input("Enter a number:"))
-b = int(input("Enter a numbter:"))
-
-print(gcf(a,b))
+Number_1=int(input("Enter your first number:"))
+Number_2=int(input("Enter your second number:"))
+for i in range(1, Number_1+1):
+    if Number_1%i==0:
+        print(i)
+for j in range(1, Number_2+1):
+    if Number_2%j==0:
+        print(j)
+list.append(i)
