40888a867580d775327f01ecbdba3a091838da0a
EllaYu310 <ellay9@nycstudents.net>
2023-09-26 23:49:59 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,23 @@
-def gcf(a,b):
-    if b==0:
-        return a
-    else:
-        return gcf(b, a%b)
-a = int(input("Enter a number:"))
-b = int(input("Enter a numbter:"))
+##Challenge Finding GCF
+#list=[]
+#def greatest_common_factor(a,b):
+#    for i in range(1, min(a,b)+1):
+#        if a%i==0 and b%i==0:
+#            list.append(i)
+#number_1=int(input("Enter your first number:"))
+#number_2=int(input("Enter your second number:"))
+#print(max(list))
 
-print(gcf(a,b))
+list=[]
+def gcf(a,b):
+    if number_1>number_2:
+        smaller=number_2
+    else:
+        smaller=number_1
+    for i in range(1, smaller+1):
+        if number_1%i==0 and number_2%i==0:
+            list.append(i)
+number_1=int(input("Enter your first number:"))
+number_2=int(input("Enter your second number:"))
+gcf(number_1,number_2)
+print(max(list))
