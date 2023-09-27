400611ec65dfd1a201e773efdb5bfb3743c3ffca
EllaYu310 <ellay9@nycstudents.net>
2023-09-27 13:49:46 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,6 @@
-list=[]
-def gcf(a,b):
-    if number_1>number_2:
-        small=number_2
-    else:
-        small=number_1
-    for i in range(1, small+1):
-        if number_1%i==0 and number_2%i==0:
-            list.append(i)
-gcf(number_1,number_2)
-print(max(list))
-            
-number_1=int(input("Enter your first number:"))
-number_2=int(input("Enter your second number:"))
+#true and false is not Truetrue and true is not false
+def traffic(eastbound,westbound):
+    if eastbound==true and westbound==true:
+        print("True")
+    elif westbound== true:
+    
