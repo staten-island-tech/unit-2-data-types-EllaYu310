84c4125afcb37ff6b388c419d6cc4b90bb7de4a0
EllaYu310 <ellay9@nycstudents.net>
2023-09-27 13:23:16 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
-Number_1=int(input("Enter your first number:"))
-Number_2=int(input("Enter your second number:"))
-for i in range(1, Number_1+1):
-    if Number_1%i==0:
-        print(i)
-for j in range(1, Number_2+1):
-    if Number_2%j==0:
-        print(j)
-list.append(i)
+list=[]
+def gcf(a,b):
+    if number_1>number_2:
+        small=number_2
+    else:
+        small=number_1
+    for i in range(1, small+1):
+        if number_1%i==0 and number_2%i==0:
+            list.append(i)
+gcf(number_1,number_2)
+print(max(list))
+            
+number_1=int(input("Enter your first number:"))
+number_2=int(input("Enter your second number:"))
