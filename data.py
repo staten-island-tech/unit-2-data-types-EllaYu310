32f08076875ef89813b3b556571cc2fd193763fe
EllaYu310 <ellay9@nycstudents.net>
2023-09-26 23:54:09 -0400

    Update data.py

--- data.py
+++ data.py
@@ -37,8 +37,16 @@ for i in range(1, factors+1):
         print(i)
 
 ##Challenge Finding GCF
+list=[]
 def GCF(a,b):
-    for i in range(1, min(a,b)+1):
-        if a%i==0 and b%i==0:
-            GCF.append(i)
-    return max(GCF)
+    if number_1>number_2:
+        smaller=number_2
+    else:
+        smaller=number_1
+    for i in range(1, smaller+1):
+        if number_1%i==0 and number_2%i==0:
+            list.append(i)
+number_1=int(input("Enter your first number:"))
+number_2=int(input("Enter your second number:"))
+GCF(number_1,number_2)
+print(max(list))
