4c7f6d615b1f029c5c86b58e10013bcbe2ead584
EllaYu310 <ellay9@nycstudents.net>
2023-09-26 23:49:05 -0400

    Update data.py

--- data.py
+++ data.py
@@ -36,5 +36,9 @@ for i in range(1, factors+1):
     if factors%i==0:
         print(i)
 
-
 ##Challenge Finding GCF
+def GCF(a,b):
+    for i in range(1, min(a,b)+1):
+        if a%i==0 and b%i==0:
+            GCF.append(i)
+    return max(GCF)
