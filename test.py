451baa9129353edbfd56b52e7c73607523c24fd0
EllaYu310 <ellay9@nycstudents.net>
2023-09-27 22:54:24 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,23 @@
-#true and false is not Truetrue and true is not false
-def traffic(eastbound,westbound):
-    if eastbound==true and westbound==true:
-        print("True")
-    elif westbound== true:
-    
+Eastbound_traffic=input("There is traffic on the Eastbound lane, True or False?:")
+Westbound_traffic=input("There is traffic on the Westbound lane, True or False?:")
+if Eastbound_traffic=="True" and Westbound_traffic=="False":
+    print("5 minutes of traffic in the eastbound lane.")
+elif Eastbound_traffic=="False" and Westbound_traffic=="True":
+    print("5 minutes of traffic in the Westbound lane.")
+else:
+    print("Error")
+
+Eastbound_traffic=input("There is traffic on the Eastbound lane, True or False?:")
+Westbound_traffic=input("There is traffic on the Westbound lane, True or False?:")
+
+if not Eastbound_traffic=="True" and Westbound_traffic=="True":
+    print("False")
+
+elif not Eastbound_traffic=="False" and Westbound_traffic=="True":
+    print("True")
+
+elif not Eastbound_traffic=="True" and Westbound_traffic=="False":
+    print("True")
+
+elif not Eastbound_traffic=="False" and Westbound_traffic=="False":
+    print("True")
