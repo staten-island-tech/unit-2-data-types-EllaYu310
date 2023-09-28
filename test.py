26d1b7a335961e967b664f6f5382ec61e20f3865
EllaYu310 <ellay9@nycstudents.net>
2023-09-28 13:42:10 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,22 @@
-Eastbound_traffic=input("There is traffic on the Eastbound lane, True or False?:")
-Westbound_traffic=input("There is traffic on the Westbound lane, True or False?:")
-if Eastbound_traffic=="True" and Westbound_traffic=="False":
-    print("5 minutes of traffic in the eastbound lane.")
-elif Eastbound_traffic=="False" and Westbound_traffic=="True":
-    print("5 minutes of traffic in the Westbound lane.")
-else:
-    print("Error")
+#Eastbound_traffic=input("There is traffic on the Eastbound lane, True or False?:")
+#Westbound_traffic=input("There is traffic on the Westbound lane, True or False?:")
+#if Eastbound_traffic=="True" and Westbound_traffic=="False":
+#    print("5 minutes of traffic in the eastbound lane.")
+#elif Eastbound_traffic=="False" and Westbound_traffic=="True":
+#    print("5 minutes of traffic in the Westbound lane.")
+#else:
+#    print("Error")
 
-Eastbound_traffic=input("There is traffic on the Eastbound lane, True or False?:")
-Westbound_traffic=input("There is traffic on the Westbound lane, True or False?:")
+#Truth table
+x=9213
+y=True
 
-if not Eastbound_traffic=="True" and Westbound_traffic=="True":
-    print("False")
-
-elif not Eastbound_traffic=="False" and Westbound_traffic=="True":
-    print("True")
-
-elif not Eastbound_traffic=="True" and Westbound_traffic=="False":
-    print("True")
-
-elif not Eastbound_traffic=="False" and Westbound_traffic=="False":
-    print("True")
+def truthy(x,y):
+    if type(x) !=bool or type(y) !=bool:
+        print("Error")
+    else:
+        if x==y:
+            print("False")
+        elif x!=y:
+            print("True")
+truthy(x,y)
